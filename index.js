22b60f1b015da0311c7c566751249ce9953c5b3c
Adam Bougaev <adambougaev@gmail.com>
2021-03-22 21:27:39 -0400

    -

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js'),
+client = new Discord.Client()
+let regex = /<@!(?<gamer>\d*)>/gm
+
+client.on('message', msg => {
+  let z = false
+  //console.log(msg.content)
+  //console.log(client.users.cache.get().toString().toLowerCase())
+  if (msg.content.match(regex) != null) {
+    msg.content.match(regex).forEach(s => {
+      s = s.substring(3, s.length - 1)
+      if (s != null) {
+        if (msg.guild.member(s).displayName.toLowerCase().includes('bonk')) {
+          z = true
+        }
+      }
+    })
+  }
+  if ((msg.content.toLowerCase().includes("bonk") || z) && msg.author.id != "823668195650961469" ) {
+    msg.channel.send("BONK")
+  }
+});
+
+client.on('ready', () => {
+  console.log('revved and ready to go')
+})
+
+client.login("ODIzNjY4MTk1NjUwOTYxNDY5.YFkKyA.cMcnULXAqU55nz6NBZsD22tppGY");
