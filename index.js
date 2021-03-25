044fe800a2d545b540a713b5bbac901f132fb1fb
Adam Bougaev <adambougaev@gmail.com>
2021-03-25 15:13:43 -0400

    epic

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const Discord = require('discord.js'),
 dotenv = require('dotenv').config(),
 client = new Discord.Client()
-let regex = /<@!(?<gamer>\d*)>/gm
 
-client.on('message', msg => {
-  let z = false
-  //console.log(msg.content)
-  //console.log(client.users.cache.get().toString().toLowerCase())
-  if (msg.content.match(regex) != null) {
-    msg.content.match(regex).forEach(s => {
-      s = s.substring(3, s.length - 1)
-      if (s != null) {
-        if (msg.guild.member(s).displayName.toLowerCase().includes('bonk')) {
-          z = true
-        }
-      }
-    })
-  }
-  if ((msg.content.toLowerCase().includes("bonk") || z) && msg.author.id != "823668195650961469" ) {
+client.on('message', async msg => {
+  if ((msg.content.toLowerCase().includes("bonk") || await isChannelBonk(msg) || isMentionBonk(msg)) && msg.author.id != "823668195650961469" ) {
     msg.channel.send("BONK")
   }
 });
@@ -28,4 +14,31 @@ client.on('ready', () => {
 
 client.login(process.env.BOT_TOKEN);
 
-//epicgames
+function isMentionBonk(msg) {
+  let regex = /<@!(?<gamer>\d*)>/gm
+  let z = false;
+  if (msg.content.match(regex) != null) {
+    msg.content.match(regex).forEach(s => {
+      s = s.substring(3, s.length - 1)
+      if (s != null) {
+        z = z || msg.guild.member(s).displayName.toLowerCase().includes('bonk')
+      }
+    })
+  }
+  return z
+}
+
+async function isChannelBonk(msg) {
+  let regex2 = /<#(?<gamer>\d*)>/gm
+  let z = false;
+  if (msg.content.match(regex2) != null) {
+    let matches = msg.content.match(regex2)
+    for (let i in matches) {
+      matches[i] = matches[i].substring(2, matches[i].length - 1)
+      if (matches[i] != null) {
+        z = z || await (await client.channels.fetch(matches[i])).toJSON().name.toLowerCase().includes("bonk")
+      }
+    }
+  }
+  return z
+}
