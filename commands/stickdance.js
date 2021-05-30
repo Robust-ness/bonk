e171eb09698eb1004746345561216bb33d1732bd
Adam Bougaev <adambougaev@gmail.com>
2021-05-29 21:58:26 -0400

    okokoko

--- commands/stickdance.js
+++ commands/stickdance.js
@@ -5,12 +5,13 @@ module.exports = {
     name: "stickdance",
     description: "dsadsa9fhs8u9fhsdf",
     async execute(client, interaction) {
-        let channel = client.channels.cache.find(channel => channel.id == interaction.channel_id)
-        let attach = new Discord.MessageAttachment('image0.gif')
-        channel.send(attach)
+        // let channel = client.channels.cache.find(channel => channel.id == interaction.channel_id)
+        // let attach = await new Discord.MessageAttachment('image0.gif')
+        // console.log(attach)
+        // channel.send(attach)
         return
     },
     responseType: 4,
     options: [],
-    response: "funky"
+    response: "https://cdn.discordapp.com/attachments/827098160345645109/848366683272904754/image0.gif"
 }
