643176d161d418b828ffe1d68ea8afc274300717
Robust-ness <adambougaev@gmail.com>
2021-08-31 13:27:48 -0400

    -

--- commands/listnerdwords.js
+++ commands/listnerdwords.js
@@ -4,10 +4,11 @@ module.exports = {
     name: "listnerdwords",
     description: "List all words that are in the nerd word dictionary.",
     async execute(client, interaction) {
+        client.channels.cache.find(channel => channel.id == interaction.channel_id).send("`" + fs.readFileSync("nerd-dictionary.txt").toString().replace(/,/g, ", ") + "`")
         return
     },
     responseType: 4,
     options: [],
-    response: "`" + fs.readFileSync("nerd-dictionary.txt").toString().replace(/,/g, ", ") + "`",
+    response: "Current List: ",
     flags: 64
 }
