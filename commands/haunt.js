9c990e407abfbe2ae9b2a349d0544d7f3e575821
nb50802 <70917695+nb50802@users.noreply.github.com>
2022-11-21 02:47:00 -0500

    Create haunt.js

---
+++ commands/haunt.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("haunt")
+        .setDescription("Go fuck yourself")
+		    .addUserOption(user =>
+            user
+                .setName("user")
+                .setDescription("figure it the fuck out")
+                .setRequired(true)
+        ),
+        .addStringOption(message =>
+            message
+                .setName("message")
+                .setDescription("figure it the fuck out")
+                .setRequired(true)
+        ),
+
+    async execute(client, interaction) {
+		    const user = interaction.options.getUser("user")
+	    	const message = interaction.options.getString("message")
+        user.send(message)
+        interaction.reply({content: "Sent!", ephemeral: true})
+    }
+}
