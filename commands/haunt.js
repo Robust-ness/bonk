d3793e4c1f7ab64cd8438841bf90785ab099265a
nb50802 <70917695+nb50802@users.noreply.github.com>
2022-11-21 02:53:37 -0500

    Update haunt.js

--- commands/haunt.js
+++ commands/haunt.js
@@ -15,12 +15,35 @@ module.exports = {
                 .setName("message")
                 .setDescription("figure it the fuck out")
                 .setRequired(true)
-        ),
+        )
+	.addIntegerOption(i =>
+		i
+		.setName("i")
+		.setDescription("repeat amount")
+		.setRequired(false)
+	),
 
     async execute(client, interaction) {
+	let amount = interaction.options.getInteger("i")
+		
+	if(amount == undefined)
+		amount = 1
+	    
 	const user = interaction.options.getUser("user")
 	const message = interaction.options.getString("message")
-        user.send(message)
-        interaction.reply({content: "Sent!", ephemeral: true})
+	
+        if (amount < 0 || amount > 10) {
+		interaction.reply({
+			ephemeral: true,
+			content: "'i' must be within 0..10"
+		})
+        }
+	else {
+		for (let i = 0; i < amount; i++) {
+			user.send(message)
+		}
+		
+		interaction.reply({content: "Sent!", ephemeral: true})
+	}
     }
 }
